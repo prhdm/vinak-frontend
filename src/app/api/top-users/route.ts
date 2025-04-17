3d114dd9fdb8a2d172bc08f5e6158b9cf7447f7c
prhdm <parhamhoushmand@outlook.com>
2025-04-17 21:32:09 +0330

    initial commit

--- src/app/api/top-users/route.ts
+++ src/app/api/top-users/route.ts
@@ -8,16 +8,16 @@ interface Supporter {
 }
 
 const supporters: Supporter[] = [
-  { name: 'Brian Etemad', instagram: 'brianetemad', amount: 12, currency: 'USD' },
-  { name: 'Alishmas', instagram: 'alishmasz', amount: 420000, currency: 'IRR' },
-  { name: 'SLP', instagram: 'slpabbas', amount: 1000, currency: 'USD' },
-  { name: 'Aria Khosravi', instagram: 'ariaa_khosravi', amount: 3000000, currency: 'IRR' },
-  { name: 'Bashir', instagram: 'bashir.official', amount: 2000000, currency: 'IRR' },
-  { name: 'Ehsan Mombeini', instagram: 'ehsanmobeiniii', amount: 50, currency: 'USD' },
-  { name: 'Erfan Eslahi', instagram: 'erfitunes', amount: 530000, currency: 'IRR' },
-  { name: 'Putak', instagram: 'braveputak', amount: 2500000, currency: 'IRR' },
-  { name: 'Catchy Beatz', instagram: 'tiktaaksr', amount: 3000, currency: 'USD' },
-  { name: 'Behzad Leito', instagram: 'behzadleito', amount: 80, currency: 'USD' },
+  // { name: 'Brian Etemad', instagram: 'brianetemad', amount: 12, currency: 'USD' },
+  // { name: 'Alishmas', instagram: 'alishmasz', amount: 420000, currency: 'IRR' },
+  // { name: 'SLP', instagram: 'slpabbas', amount: 1000, currency: 'USD' },
+  // { name: 'Aria Khosravi', instagram: 'ariaa_khosravi', amount: 3000000, currency: 'IRR' },
+  // { name: 'Bashir', instagram: 'bashir.official', amount: 2000000, currency: 'IRR' },
+  // { name: 'Ehsan Mombeini', instagram: 'ehsanmobeiniii', amount: 50, currency: 'USD' },
+  // { name: 'Erfan Eslahi', instagram: 'erfitunes', amount: 530000, currency: 'IRR' },
+  // { name: 'Putak', instagram: 'braveputak', amount: 2500000, currency: 'IRR' },
+  // { name: 'Catchy Beatz', instagram: 'tiktaaksr', amount: 3000, currency: 'USD' },
+  // { name: 'Behzad Leito', instagram: 'behzadleito', amount: 80, currency: 'USD' },
 ];
 
 export async function GET() {
